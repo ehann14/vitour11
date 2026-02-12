beac9765473b74407d911d22eec49492ea026bee
ehann14 <ferhan142009@gmail.com>
2026-02-12 14:23:58 +0700

    add splash screen

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // Splash screen
 });
+
+Route::get('/home', function () {
+    return view('home'); // Halaman utama
+})->name('home');
