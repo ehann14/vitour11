627cf550ddd09414722f93bc88f8c72f547eb52c
ehann14 <ferhan142009@gmail.com>
2026-05-07 11:59:34 +0700

    update final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>SMK Negeri 11 Bandung - Sekolah Unggulan</title>
+    <title>SMK Negeri 11 Bandung</title>
     <link rel="icon" type="image/png" href="{{ asset('image/b/SMK11.jpeg') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
